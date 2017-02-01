fe08d2a63e599f1ba478968b2536a892c3505275
mudrz <mudrz@outlook.com>
2017-02-01 11:49:41 +0200

    spread

---
+++ files_to_test/spread.js
@@ -0,0 +1,11 @@
+const foo = {
+  bar: 'bar'
+}
+
+const baz = {
+  ...foo,
+  bar2: 'bar2'
+}
+
+console.log('foo', foo)
+console.log('baz', baz)
---
+++ files_to_test/spread.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul>
+        <li>some</li>
+        <li>another</li>
+    </ul>
+</template>
+<script>
+const foo = {
+  bar: 'bar'
+}
+
+const baz = {
+  ...foo,
+  bar2: 'bar2'
+}
+
+console.log('foo', foo)
+console.log('baz', baz)
+</script>
